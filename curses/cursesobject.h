871709882cf0c4a599701efa0893ad47b444e619
Damian Korzekwa <damian.korzekwa@gmail.com>
2018-04-21 17:36:16 +0200

    Add mutex

--- curses/cursesobject.h
+++ curses/cursesobject.h
@@ -2,12 +2,17 @@
 #define CURSESOBJECT_H
 
 
+#include <mutex>
+
 class CursesObject
 {
 public:
     virtual void print() = 0;
     virtual void moveTo(int x, int y) = 0;
     virtual void update() = 0;
+
+private:
+    std::mutex objectMutex;
 };
 
 #endif // CURSESOBJECT_H
